c68ef113dc6d591ec917d42702a236095a7a84b9
ii-batman <60931858+ii-batman@users.noreply.github.com>
2021-06-21 20:54:05 +0100

    Update Win10_VirtualDesktop_Optimize.ps1

--- Win10_VirtualDesktop_Optimize.ps1
+++ Win10_VirtualDesktop_Optimize.ps1
@@ -252,7 +252,7 @@ PROCESS {
             If ($UserSettings.Count -gt 0) {
                 Write-Verbose "Processing Default User Settings (Registry Keys)"
 
-                & REG LOAD HKLM\DEFAULT C:\Users\Default\NTUSER.DAT | Out-Null
+                & REG LOAD HKLM\VDOT_TEMP C:\Users\Default\NTUSER.DAT 
 
                 Foreach ($Item in $UserSettings) {
                     If ($Item.PropertyType -eq "BINARY") { $Value = [byte[]]($Item.PropertyValue.Split(",")) }
@@ -272,7 +272,7 @@ PROCESS {
                     }
                 }
 
-                & REG UNLOAD HKLM\DEFAULT | Out-Null
+                & REG UNLOAD HKLM\VDOT_TEMP 
             }
             Else { Write-Warning ("No Default User Settings to set") }
         }
